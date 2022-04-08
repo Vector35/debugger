7b66063d6345ff6bf1e6e02838ae24bb19073e02
Xusheng <xusheng@vector35.com>
2022-04-08 12:24:06 +0800

    Prompt the user to terminate the target when the debugger is active and the user closes the tab/window

--- ui/uinotification.cpp
+++ ui/uinotification.cpp
@@ -4,6 +4,8 @@
 #include "viewframe.h"
 #include <QMessageBox>
 #include <QFileInfo>
+#include <QPushButton>
+#include <QObject>
 #include "ui.h"
 
 using namespace BinaryNinja;
@@ -63,6 +65,55 @@ void NotificationListener::OnAfterSaveFile(UIContext* context, FileContext* file
 
 bool NotificationListener::OnBeforeCloseFile(UIContext* context, FileContext* file, ViewFrame* frame)
 {
+	auto currentTab = context->getCurrentTab();
+	auto mainWindow = context->mainWindow();
+	auto tabs = context->getTabs();
+	bool tabRemaining = false;
+	for (auto tab: tabs)
+	{
+		if (tab == currentTab)
+			continue;
+
+		auto viewFrame = context->getViewFrameForTab(tab);
+		if (viewFrame && (viewFrame->getFileContext() == file))
+		{
+			tabRemaining = true;
+			break;
+		}
+	}
+
+	// This is the last tab of the file being closed. Check whether the debugger is connected
+	if (!tabRemaining)
+	{
+		auto viewFrame = context->getCurrentViewFrame();
+		if (!viewFrame)
+			return true;
+		auto data = viewFrame->getCurrentBinaryView();
+		if (!data)
+			return true;
+		auto controller = DebuggerController::GetController(data);
+		if (controller && controller->IsConnected())
+		{
+			QMessageBox* msgBox = new QMessageBox(mainWindow);
+			msgBox->setAttribute(Qt::WA_DeleteOnClose);
+			msgBox->setIcon(QMessageBox::Question);
+			msgBox->setText(QObject::tr("The debugger file ") + file->getShortFileName(mainWindow) + QObject::tr(" is active. Do you want to stop it before closing?"));
+			msgBox->setWindowTitle(QObject::tr("Debugger Active"));
+			msgBox->setStandardButtons(QMessageBox::Yes | QMessageBox::Cancel);
+			msgBox->setDefaultButton(QMessageBox::Yes);
+			msgBox->show();
+			msgBox->move(mainWindow->frameGeometry().center() - msgBox->rect().center());
+			msgBox->setAttribute(Qt::WA_KeyboardFocusChange);
+			int result = msgBox->exec();
+			if (result == QMessageBox::Cancel)
+				return false;
+			else if (result == QMessageBox::Yes)
+			{
+				controller->Quit();
+				return true;
+			}
+		}
+	}
 	return true;
 }
 
