e8baa69e2b33b206151912e652e299c79db90391
Xusheng <xusheng@vector35.com>
2022-04-13 11:30:15 +0800

    Add copyright notice to every source files. Fix https://github.com/Vector35/debugger_native/issues/120

--- core/adapters/lldbadapter.cpp
+++ core/adapters/lldbadapter.cpp
@@ -111,7 +111,23 @@ bool LldbAdapter::Connect(const std::string & server, std::uint32_t port)
 	// Hacky way to supply the path info into the LLDB
 	m_target = m_debugger.CreateTarget(m_data->GetFile()->GetOriginalFilename().c_str());
 	if (!m_target.IsValid())
-		return false;
+		return false;/*
+                     Copyright 2020-2022 Vector 35 Inc.
+
+                     Licensed under the Apache License, Version 2.0 (the "License");
+                     you may not use this file except in compliance with the License.
+                     You may obtain a copy of the License at
+
+                     http://www.apache.org/licenses/LICENSE-2.0
+
+                     Unless required by applicable law or agreed to in writing, software
+                     distributed under the License is distributed on an "AS IS" BASIS,
+                     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+                     See the License for the specific language governing permissions and
+                     limitations under the License.
+                     */
+
+
 
 	std::string url = fmt::format("connect://{}:{}", server, port);
 	SBError error;
