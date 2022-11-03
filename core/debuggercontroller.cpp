41674f018c9b5ca3472083864d1c4cb79580d4b7
op2786 <op2786@proton.me>
2022-11-03 09:30:12 +0100

    tab fix

--- core/debuggercontroller.cpp
+++ core/debuggercontroller.cpp
@@ -90,14 +90,14 @@ bool DebuggerController::SetIP(uint64_t address)
 	std::string ipRegisterName;
 	std::string targetArch = GetRemoteArchitecture()->GetName();
 
-    if ((targetArch == "x86") || (targetArch == "i386"))
-        ipRegisterName = "eip";
-    else if (targetArch == "x86_64")
-        ipRegisterName = "rip";
-    else if ((targetArch == "aarch64") || (targetArch == "arm64"))
-        ipRegisterName = "pc";
-    else
-        ipRegisterName = "pc";
+	if ((targetArch == "x86") || (targetArch == "i386"))
+		ipRegisterName = "eip";
+	else if (targetArch == "x86_64")
+		ipRegisterName = "rip";
+	else if ((targetArch == "aarch64") || (targetArch == "arm64"))
+		ipRegisterName = "pc";
+	else
+		ipRegisterName = "pc";
 
 	if (!SetRegisterValue(ipRegisterName, address))
 		return false;
