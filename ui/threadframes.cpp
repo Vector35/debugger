e7dd08095554a9d3798f361e2c32ec46b11e576e
op2786 <op2786@proton.me>
2022-11-09 09:52:49 +0100

    Convert the stack frames widget to a table. Support navigating to ip, sp, and fp from it. Fix https://github.com/Vector35/debugger/issues/325

--- ui/threadframes.cpp
+++ ui/threadframes.cpp
@@ -19,16 +19,270 @@ limitations under the License.
 #include "debuggerapi.h"
 #include "inttypes.h"
 
-using namespace BinaryNinja;
-using namespace BinaryNinjaDebuggerAPI;
+
+constexpr int SortFilterRole = Qt::UserRole + 1;
+
+FrameItem::FrameItem(int index, std::string module, std::string function, uint64_t pc, uint64_t sp, uint64_t fp):
+	m_frameIndex(index), m_module(module), m_function(function), m_pc(pc), m_sp(sp), m_fp(fp)
+{
+}
+
+
+bool FrameItem::operator==(const FrameItem& other) const
+{
+	return (m_module == other.module()) && (m_function == other.function()) && (m_pc == other.pc()) &&
+		(m_fp == other.fp()) && (m_sp == other.sp());
+}
+
+
+bool FrameItem::operator!=(const FrameItem& other) const
+{
+	return !(*this == other);
+}
+
+
+bool FrameItem::operator<(const FrameItem& other) const
+{
+	if (m_module < other.module())
+		return true;
+	else if (m_module > other.module())
+		return false;
+	else if (m_function < other.function())
+		return true;
+	else if (m_function > other.function())
+		return false;
+	else if (m_pc < other.pc())
+		return true;
+	else if (m_pc > other.pc())
+		return false;
+	else if (m_sp < other.sp())
+		return true;
+	else if (m_sp > other.sp())
+		return false;
+	return m_fp < other.fp();
+}
+
+
+ThreadFramesListModel::ThreadFramesListModel(QWidget* parent, ViewFrame* view):
+	QAbstractTableModel(parent), m_view(view)
+{
+}
+
+
+ThreadFramesListModel::~ThreadFramesListModel()
+{
+}
+
+
+FrameItem ThreadFramesListModel::getRow(int row) const
+{
+	if ((size_t)row >= m_items.size())
+		throw std::runtime_error("row index out-of-bound");
+
+	return m_items[row];
+}
+
+
+QModelIndex ThreadFramesListModel::index(int row, int column, const QModelIndex &) const
+{
+	if (row < 0 || (size_t)row >= m_items.size() || column >= columnCount())
+	{
+		return QModelIndex();
+	}
+
+	return createIndex(row, column, (void*)&m_items[row]);
+}
+
+
+QVariant ThreadFramesListModel::data(const QModelIndex& index, int role) const
+{
+	if (index.column() >= columnCount() || (size_t)index.row() >= m_items.size())
+		return QVariant();
+
+	FrameItem *item = static_cast<FrameItem*>(index.internalPointer());
+	if (!item)
+		return QVariant();
+
+	if ((role != Qt::DisplayRole) && (role != Qt::SizeHintRole) && (role != SortFilterRole))
+		return QVariant();
+
+	switch (index.column())
+	{
+	case ThreadFramesListModel::IndexColumn:
+	{
+		QString text = QString::asprintf("%d", item->frameIndex());
+		if (role == Qt::SizeHintRole)
+			return QVariant((qulonglong)text.size());
+
+		return QVariant(text);
+	}
+	case ThreadFramesListModel::ModuleColumn:
+	{
+		QString text = QString::fromStdString(item->module());
+		if (role == Qt::SizeHintRole)
+			return QVariant((qulonglong)text.size());
+
+		return QVariant(text);
+	}
+	case ThreadFramesListModel::FunctionColumn:
+	{
+		QString text = QString::fromStdString(item->function());
+		if (role == Qt::SizeHintRole)
+			return QVariant((qulonglong)text.size());
+
+		return QVariant(text);
+	}
+	case ThreadFramesListModel::PcColumn:
+	{
+		QString text = QString::asprintf("0x%" PRIx64, item->pc());
+		if (role == Qt::SizeHintRole)
+			return QVariant((qulonglong)text.size());
+
+		return QVariant(text);
+	}
+	case ThreadFramesListModel::SpColumn:
+	{
+		QString text = QString::asprintf("0x%" PRIx64, item->sp());
+		if (role == Qt::SizeHintRole)
+			return QVariant((qulonglong)text.size());
+
+		return QVariant(text);
+	}
+	case ThreadFramesListModel::FpColumn:
+	{
+		QString text = QString::asprintf("0x%" PRIx64, item->fp());
+		if (role == Qt::SizeHintRole)
+			return QVariant((qulonglong)text.size());
+
+		return QVariant(text);
+	}
+	}
+	return QVariant();
+}
+
+
+QVariant ThreadFramesListModel::headerData(int column, Qt::Orientation orientation, int role) const
+{
+	if (role != Qt::DisplayRole)
+		return QVariant();
+
+	if (orientation == Qt::Vertical)
+		return QVariant();
+
+	switch (column)
+	{
+	case ThreadFramesListModel::IndexColumn:
+		return "#";
+	case ThreadFramesListModel::ModuleColumn:
+		return "Module";
+	case ThreadFramesListModel::FunctionColumn:
+		return "Function";
+	case ThreadFramesListModel::PcColumn:
+		return "PC";
+	case ThreadFramesListModel::SpColumn:
+		return "SP";
+	case ThreadFramesListModel::FpColumn:
+		return "FP";
+	}
+	return QVariant();
+}
+
+
+void ThreadFramesListModel::updateRows(std::vector<BinaryNinjaDebuggerAPI::DebugFrame> frames)
+{
+	beginResetModel();
+
+	std::vector<FrameItem> newRows;
+	for (const DebugFrame& frame: frames)
+	{
+		uint64_t offset = frame.m_pc - frame.m_functionStart;
+		QString funcName = QString::asprintf("%s + 0x%" PRIx64, frame.m_functionName.c_str(), offset);
+
+		newRows.emplace_back((int)frame.m_index, frame.m_module, funcName.toStdString(), frame.m_pc, frame.m_sp, frame.m_fp);
+	}
+
+	m_items = newRows;
+	endResetModel();
+}
+
+
+ThreadFramesItemDelegate::ThreadFramesItemDelegate(QWidget* parent):
+	QStyledItemDelegate(parent)
+{
+	updateFonts();
+}
+
+
+void ThreadFramesItemDelegate::paint(QPainter* painter, const QStyleOptionViewItem& option,
+	const QModelIndex& idx) const
+{
+	painter->setFont(m_font);
+
+	bool selected = (option.state & QStyle::State_Selected) != 0;
+	if (selected)
+		painter->setBrush(getThemeColor(SelectionColor));
+	else
+		painter->setBrush(option.backgroundBrush);
+
+	painter->setPen(Qt::NoPen);
+	painter->setFont(m_font);
+
+	QRect textRect = option.rect;
+	textRect.setBottom(textRect.top() + m_charHeight + 2);
+	painter->drawRect(textRect);
+
+	auto data = idx.data(Qt::DisplayRole);
+	switch (idx.column())
+	{
+	case ThreadFramesListModel::IndexColumn:
+	{
+		painter->setPen(getThemeColor(NumberColor).rgba());
+		painter->drawText(textRect, data.toString());
+		break;
+	}
+	case ThreadFramesListModel::ModuleColumn:
+	case ThreadFramesListModel::FunctionColumn:
+	{
+		painter->setPen(option.palette.color(QPalette::WindowText).rgba());
+		painter->drawText(textRect, data.toString());
+		break;
+	}
+	case ThreadFramesListModel::PcColumn:
+	case ThreadFramesListModel::FpColumn:
+	case ThreadFramesListModel::SpColumn:
+	{
+		painter->setPen(getThemeColor(AddressColor).rgba());
+		painter->drawText(textRect, data.toString());
+		break;
+	}
+	default:
+		break;
+	}
+}
+
+
+void ThreadFramesItemDelegate::updateFonts()
+{
+	// Get font and compute character sizes
+	m_font = getMonospaceFont(dynamic_cast<QWidget*>(parent()));
+	m_font.setKerning(false);
+	m_baseline = (int)QFontMetricsF(m_font).ascent();
+	m_charWidth = getFontWidthAndAdjustSpacing(m_font);
+	m_charHeight = (int)(QFontMetricsF(m_font).height() + getExtraFontSpacing());
+	m_charOffset = getFontVerticalOffset();
+}
+
+
+QSize ThreadFramesItemDelegate::sizeHint(const QStyleOptionViewItem& option, const QModelIndex& idx) const
+{
+	auto totalWidth = (idx.data(Qt::SizeHintRole).toInt() + 2) * m_charWidth + 4;
+	return QSize(totalWidth, m_charHeight + 2);
+}
 
 ThreadFramesWidget::ThreadFramesWidget(QWidget* parent, ViewFrame* frame, BinaryViewRef data):
 	QWidget(parent), m_view(frame)
 {
 	m_debugger = DebuggerController::GetController(data);
-	// In rare cases, m_debugger could be nullptr. This only happens when 1. a target exits and the live view of the
-	// DebuggerController is set to nullptr, 2. the UI still shows the live view. This is a short time span, since
-	// the debugger UI will reopen the file context after the target exits, which no longer contains teh live view.
 	if (!m_debugger)
 		return;
 
@@ -38,41 +292,42 @@ ThreadFramesWidget::ThreadFramesWidget(QWidget* parent, ViewFrame* frame, Binary
 	setFont(getMonospaceFont(this));
 
 	m_threadList = new QComboBox(this);
-	m_threadFrames = new QListWidget(this);
+
+	m_threadFramesTable = new QTableView(this);
+	m_model = new ThreadFramesListModel(m_threadFramesTable, frame);
+	
+	m_threadFramesTable->setModel(m_model);
+	m_threadFramesTable->setShowGrid(false);
+
+	m_delegate = new ThreadFramesItemDelegate(this);
+	m_threadFramesTable->setItemDelegate(m_delegate);
+
+	m_threadFramesTable->setSelectionBehavior(QAbstractItemView::SelectItems);
+
+	m_threadFramesTable->verticalHeader()->setSectionResizeMode(QHeaderView::ResizeToContents);
+	m_threadFramesTable->verticalHeader()->setVisible(false);
+
+	m_threadFramesTable->setHorizontalScrollMode(QAbstractItemView::ScrollPerPixel);
+	m_threadFramesTable->setVerticalScrollMode(QAbstractItemView::ScrollPerPixel);
+
+	m_threadFramesTable->resizeColumnsToContents();
+	m_threadFramesTable->resizeRowsToContents();
 
 	layout->addWidget(new QLabel("Thread:"));
 	layout->addWidget(m_threadList);
-	layout->addWidget(m_threadFrames);
+	layout->addWidget(m_threadFramesTable);
 	setLayout(layout);
 
 	// Set up colors
 	QPalette widgetPalette = this->palette();
+
+	connect(m_threadFramesTable, &QTableView::doubleClicked, this, &ThreadFramesWidget::onDoubleClicked);
 
 	connect(m_threadList, &QComboBox::activated, [&](int index){
 		uint32_t tid = m_threadList->currentData().toInt();
 		uint32_t currentTid = m_debugger->GetActiveThread().m_tid;
 		if (tid != currentTid)
 			m_debugger->SetActiveThread(tid);
-	});
-
-	connect(m_threadFrames, &QListWidget::itemDoubleClicked, [&](QListWidgetItem* item){
-		std::string text = item->text().toStdString();
-		auto pos = text.find("0x");
-		if (pos == std::string::npos)
-			return;
-
-		text = text.substr(pos, text.length());
-		pos = text.find(" ");
-		if (pos != std::string::npos)
-			text = text.substr(0, pos);
-
-		uint64_t address = strtoull(text.c_str(), nullptr, 16);
-		if (address != 0)
-		{
-			UIContext* context = UIContext::contextForWidget(this);
-			ViewFrame* frame = context->getCurrentViewFrame();
-			frame->navigate(m_debugger->GetLiveView(), address, true, true);
-		}
 	});
 
 	m_debuggerEventCallback = m_debugger->RegisterEventCallback([&](const DebuggerEvent& event){
@@ -118,16 +373,49 @@ void ThreadFramesWidget::updateContent()
 	m_threadList->setCurrentIndex(index);
 
 	std::vector<DebugFrame> frames = m_debugger->GetFramesOfThread(activeThread.m_tid);
-	m_threadFrames->clear();
-	for (const DebugFrame& frame: frames)
+	m_model->updateRows(frames);
+	m_threadFramesTable->resizeColumnsToContents();
+}
+
+
+void ThreadFramesWidget::onDoubleClicked()
+{
+	QModelIndexList sel = m_threadFramesTable->selectionModel()->selectedIndexes();
+	if (sel.empty())
+		return;
+
+	if (sel[0].column() < ThreadFramesListModel::FunctionColumn)
+		return;
+
+	auto frameItem = m_model->getRow(sel[0].row());
+
+	uint64_t addrToJump = 0;
+	switch (sel[0].column())
 	{
-		QString text = QString::asprintf("#%d: 0x%" PRIx64, (int)frame.m_index, frame.m_pc);
-		uint64_t offset = frame.m_pc - frame.m_functionStart;
-		QString symbolizedInfo = QString::asprintf("%s`%s + 0x%" PRIx64 ", sp: 0x%" PRIx64 ", fp: 0x%" PRIx64,
-												   frame.m_module.c_str(), frame.m_functionName.c_str(), offset,
-												   frame.m_sp, frame.m_fp);
-		m_threadFrames->addItem(text + ' ' + symbolizedInfo);
+	case ThreadFramesListModel::FunctionColumn:
+	case ThreadFramesListModel::PcColumn:
+		addrToJump = frameItem.pc();
+		break;
+	case ThreadFramesListModel::SpColumn:
+		addrToJump = frameItem.sp();
+		break;
+	case ThreadFramesListModel::FpColumn:
+		addrToJump = frameItem.fp();
+		break;
 	}
+
+	UIContext* context = UIContext::contextForWidget(this);
+	if (!context)
+		return;
+
+	ViewFrame* frame = context->getCurrentViewFrame();
+	if (!frame)
+		return;
+
+	if (m_debugger->GetLiveView())
+		frame->navigate(m_debugger->GetLiveView(), addrToJump, true, true);
+	else
+		frame->navigate(m_debugger->GetData(), addrToJump, true, true);
 }
 
 
